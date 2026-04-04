9f3cc72bb45e225db75f93b7bf92380d6e33c6d5
LoneWolfLedger <77krsna@gmail.com>
2026-04-04 22:59:25 +0000

    Saved layout file

--- lone-wolf_frontend/app/layout.tsx
+++ lone-wolf_frontend/app/layout.tsx
@@ -1,14 +1,20 @@
-export const metadata = {
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
   title: 'LONEWOLF: OMNI UI',
   description: 'The Technological Singularity',
-}
+};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body style={{ margin: 0, padding: 0, backgroundColor: 'black' }}>
         {children}
       </body>
     </html>
-  )
+  );
 }
